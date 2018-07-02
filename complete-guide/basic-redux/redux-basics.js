3407a6da3095a4a70d9f5d3262cbd2b5b36f85cf
nilsonmolina <nilson13@me.com>
2018-07-02 14:17:40 -0700

    side redux project completed

---
+++ complete-guide/basic-redux/redux-basics.js
@@ -0,0 +1,43 @@
+/* ------------------------------------------------------------------------
+    You can run this file using node with the following terminal command:
+        $ node redux-basics.js
+------------------------------------------------------------------------ */
+// NODE SYNTAX SINCE WE ARE USING NODE AND NOT REACT IN THIS EXAMPLE
+const redux = require('redux');
+const createStore = redux.createStore;
+
+const initialState = {
+    counter: 0
+}
+
+// Reducer 
+const rootReducer = (state = initialState, action) => {
+    if (action.type === 'INC_COUNTER') {
+        return {
+            ...state,
+            counter: state.counter + 1
+
+        };
+    }
+    if (action.type === 'ADD_COUNTER') {
+        return {
+            ...state,
+            counter: state.counter + action.value
+        };
+    }
+    return state;
+} 
+
+// Store
+const store = createStore(rootReducer);
+console.log(store.getState());
+
+// Subscription
+store.subscribe(() => {
+    console.log('[Subscription]', store.getState());
+});
+
+// Dispatching Action
+store.dispatch({type: 'INC_COUNTER'});
+store.dispatch({type: 'ADD_COUNTER', value: 10});
+console.log(store.getState());
